4842873a5eb96b6f3864fe0e90888a1dc71238a9
Javi Fontan <jfontan@gmail.com>
2022-05-26 04:18:05 +0200

    Implement FileScanner

--- gomm/scanner.go
+++ gomm/scanner.go
@@ -1,6 +1,12 @@
 package gomm
 
-import "io"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
 
 type Scanner interface {
 	Next() (string, error)
@@ -27,4 +33,41 @@ func (s *MemoryScanner) Next() (string, error) {
 	s.pos++
 
 	return l, nil
+}
+
+type FileScanner struct {
+	f   *os.File
+	b   *bufio.Reader
+	eof bool
+}
+
+func NewFileScanner(file string) (*FileScanner, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file %s: %w", file, err)
+	}
+
+	b := bufio.NewReader(f)
+
+	return &FileScanner{
+		f: f,
+		b: b,
+	}, nil
+}
+
+func (s *FileScanner) Next() (string, error) {
+	line, err := s.b.ReadString('\n')
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			s.eof = true
+			if len(line) > 0 {
+				return line, nil
+			}
+			_ = s.f.Close()
+			return "", io.EOF
+		}
+		_ = s.f.Close()
+		return "", fmt.Errorf("could not read file: %w", err)
+	}
+	return line, nil
 }
--- gomm/scanner_test.go
+++ gomm/scanner_test.go
@@ -2,6 +2,7 @@ package gomm
 
 import (
 	"io"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -19,6 +20,60 @@ func TestMemoryScanner(t *testing.T) {
 
 	var got []string
 	var err error
+	var line string
+	for {
+		line, err = s.Next()
+		if line != "" {
+			require.NoError(t, err)
+		} else {
+			require.ErrorIs(t, err, io.EOF)
+			break
+		}
+
+		got = append(got, line)
+	}
+
+	require.Equal(t, expected, got)
+}
+
+var ()
+
+func TestFileScanner(t *testing.T) {
+	contents := `1
+2
+3
+4
+5
+6
+7
+8
+9
+`
+
+	expected := []string{
+		"1\n",
+		"2\n",
+		"3\n",
+		"4\n",
+		"5\n",
+		"6\n",
+		"7\n",
+		"8\n",
+		"9\n",
+	}
+
+	f, err := os.CreateTemp("", "goom-")
+	require.NoError(t, err)
+	defer f.Close()
+
+	n, err := f.WriteString(contents)
+	require.NoError(t, err)
+	require.Equal(t, len(contents), n)
+
+	s, err := NewFileScanner(f.Name())
+	require.NoError(t, err)
+
+	var got []string
 	var line string
 	for {
 		line, err = s.Next()
