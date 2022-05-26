7140dc44d77e9a470e2f776567cadcf6b98a5cad
Javi Fontan <jfontan@gmail.com>
2022-05-26 05:19:18 +0200

    Fixed fuzzing test

--- fuzzing/fuzzing_test.go
+++ fuzzing/fuzzing_test.go
@@ -1,6 +1,7 @@
 package fuzzing
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/jfontan/gomm/gomm"
@@ -11,12 +12,20 @@ func FuzzGomm(f *testing.F) {
 	f.Add("one", "two", "three", "four")
 
 	f.Fuzz(func(t *testing.T, a1, a2, b1, b2 string) {
-		l := gomm.NewMemoryScanner([]string{a1, a2})
-		r := gomm.NewMemoryScanner([]string{b1, b2})
+		ll := []string{a1, a2}
+		sort.Strings(ll)
+		l := gomm.NewMemoryScanner(ll)
+
+		rl := []string{b1, b2}
+		sort.Strings(rl)
+		r := gomm.NewMemoryScanner(rl)
 
 		var total int
 		g := gomm.New(l, r, func(p gomm.Position, s string) {
 			total++
+			if p == gomm.BOTH {
+				total++
+			}
 		})
 
 		err := g.Compare()
